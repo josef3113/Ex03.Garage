530596ac7c27c5c59527732908ec83dad4fffc81
josef3113 <33261454+josef3113@users.noreply.github.com>
2018-04-26 16:03:13 +0300

    Add project files.

---
+++ Project2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAA
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("Hello and you add me bye bye plz");
+
+            Console.ReadLine();
+        }
+    }
+}
